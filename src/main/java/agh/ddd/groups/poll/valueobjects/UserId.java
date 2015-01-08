1ed3aaf14b8dfa5cff0c516a2ec92bf6cb2e7bf9
Michał Janczykowski <mjanczykowski@gmail.com>
2015-01-08 20:33:13 +0100

    user id and poll finished event

---
+++ src/main/java/agh/ddd/groups/poll/valueobjects/UserId.java
@@ -0,0 +1,40 @@
+package agh.ddd.groups.poll.valueobjects;
+
+/**
+ * @author Michal Janczykowski
+ */
+public class UserId {
+    private final long id;
+
+    public UserId(long id) {
+        this.id = id;
+    }
+
+    public static UserId of(long id) {
+        return new UserId(id);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserId userId = (UserId) o;
+
+        if (id != userId.id) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (id ^ (id >>> 32));
+    }
+
+    @Override
+    public String toString() {
+        return "UserId{" +
+                "id=" + id +
+                '}';
+    }
+}
